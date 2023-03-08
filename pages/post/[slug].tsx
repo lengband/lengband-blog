bce86a2cb0f941713a9a417d6663a6daad62c39e
peng.wang <peng.wang@okg.com>
2023-03-08 11:58:38 +0800

    feat: add tag

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -23,11 +23,15 @@ marked.setOptions({
 
 
 const Home: NextPage = ({ content, postEntry }: any) => {
+  const tags = postEntry?.fields?.tags?.length ? postEntry?.fields?.tags.map((tag: any) => <span className='whitespace-nowrap rounded-lg bg-slate-100 py-1 px-2 mr-2 text-xs font-semibold leading-6 text-slate-700' key={tag.fields.slug}>{tag.fields.name}</span>) : ''
   
   return (
     <article>
       <h1>
-        <div>{postEntry.fields.title}</div>
+        <div className="title-wrap">
+          <div>{postEntry.fields.title}</div>
+          <div className="tags">{tags}</div>
+        </div>
         <div className="my-4 flex items-center text-sm text-gray-500">
           <svg className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M5.75 2a.75.75 0 01.75.75V4h7V2.75a.75.75 0 011.5 0V4h.25A2.75 2.75 0 0118 6.75v8.5A2.75 2.75 0 0115.25 18H4.75A2.75 2.75 0 012 15.25v-8.5A2.75 2.75 0 014.75 4H5V2.75A.75.75 0 015.75 2zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75z" clip-rule="evenodd" />
